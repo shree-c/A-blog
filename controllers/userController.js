ff2982b80abb9ed5a2eb15dac6e9c0b43cf41188
shree <mail@shree.com>
2021-09-03 08:49:26 +0530

    completed sanitization of user inputed data for registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,25 @@
+// these functions are going to be used in router
 const User = require('../model/User')
+//going to load the home page for a user
 exports.home = function (req, res) {
-  const user = new User()
-  console.log(user)
-  res.render('home-guest')
+    //rendering the ejs file
+    res.render('home-guest');
 }
 exports.register = function (req, res) {
-  console.log(req.body)
-  res.send('thanks for registering')
+    //we are sending the entered data to User model for user objec creation
+    const user = new User(req.body);
+    //register function calls validate function on the prototype of User to validate user data
+    user.register();
+    if (user.errors.length)
+        res.send(`${user.errors}`);
+    else
+        res.send('thanks for registering')
+}
+
+exports.login = function () {
+
+}
+
+exports.logout = function () {
+
 }
--- model/User.js
+++ model/User.js
@@ -1,4 +1,51 @@
-const User = function () {
-  this.place = 'earth'
+//we are using validator npm package to validate user input data
+const validator = require('validator')
+//what defines a user?
+const User = function (data) {
+    this.data = data;
+    // for managing errors
+    this.errors = [];
 }
+
+//function to make sure the user has submitted only strings
+User.prototype.cleanUp = function () {
+    if (typeof(this.data.username) != 'string')
+        this.data.username = '';
+    if (typeof(this.data.email) != 'string')
+        this.data.email = '';
+    if (typeof(this.data.password) != 'string')
+        this.data.password = '';
+    //get rid of any other properties other than the above three
+    this.data = {
+        username : this.data.username.trim().toLowerCase(),
+        password : this.data.password,
+        email : this.data.email.trim().toLowerCase()
+    }
+}
+
+//function to validate user data
+User.prototype.validate = function () {
+    if (!this.data.username)
+        this.errors.push('empty username');
+    if (this.data.username != '' && !validator.isAlphanumeric(this.data.username))
+        this.errors.push('the username should only contain alphanumeric characters');
+    if (!validator.isEmail(this.data.email))
+        this.errors.push('not a valid email address');
+    if (!this.data.password)
+        this.errors.push('empty password');
+    if (!this.data.email)
+        this.errors.push('empty email');
+    if (this.data.password.length < 6 || this.data.password.length > 30)
+        this.errors.push('the password length should lie between 6 to 30!');
+    if (this.data.username.length < 3 || this.data.password.length > 20)
+        this.errors.push('the username length should lie between 3 to 20!');
+}
+
+User.prototype.register = function () {
+    //making sure that the user doesn't send anything other than a string
+    this.cleanUp()
+    //validating user data
+    this.validate();
+}
+
 module.exports = User
