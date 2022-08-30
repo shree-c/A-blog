b9d26e14e5e1f00b77cbc3f44830a661e54cea4d
shree <sammmmmu123@gmail.com>
2022-08-30 13:47:13 +0530

    minor changes

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const connectionString = global.process.env.DBCONSTR;
 const client = new MongoClient(connectionString);
 
-
+const PORT = process.env.PORT || 90;
 
 async function run() {
     try {
@@ -15,7 +15,7 @@ async function run() {
         module.exports = client;
         const app = require('./app');
         app.listen(global.process.env.PORT, () => {
-            console.log(`listining at ${process.env.PORT}`);
+            console.log(`listining at ${PORT}`);
         });
     } catch (err) {
         console.log(err);
